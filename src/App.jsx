90347c1f1d2f880401ff3df3711386148a1eeedf
Ítalo <italooo.senai@gmail.com>
2025-03-19 09:06:27 -0300

    import do bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Footer from "./components/footer/Footer";
 import MovieCard from "./components/movieCard/MovieCard";
 import Logo from "./assets/devflix.png";
 import Lupa from "./assets/search.svg";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.js";
 
 const App = () => {
   const [search, setSearch] = useState("");
